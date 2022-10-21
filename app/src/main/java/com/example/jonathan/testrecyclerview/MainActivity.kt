9b94d95d4dd7976e9bea03a3720d1dfc0f21b608
Jonathan Zhong <33461789+jonathanzho@users.noreply.github.com>
2022-10-21 11:10:05 -0700

    SimpleAdapter::getItemViewType() has to be manually added.

--- app/src/main/java/com/example/jonathan/testrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/jonathan/testrecyclerview/MainActivity.kt
@@ -1,26 +1,25 @@
 package com.example.jonathan.testrecyclerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import java.util.*
-import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var adapter: SimpleAdapter = SimpleAdapter(generateSimpleList())
-        var recyclerView: RecyclerView = findViewById(R.id.simple_recyclerview)
+        val adapter = SimpleAdapter(generateSimpleList())
+        val recyclerView: RecyclerView = findViewById(R.id.simple_recyclerview)
         recyclerView.setHasFixedSize(true)
-        recyclerView.setLayoutManager(LinearLayoutManager(this))
+        recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
     }
 
     private fun generateSimpleList(): List<ItemModel> {
-        var itemModelList: MutableList<ItemModel> = ArrayList<ItemModel>();
+        val itemModelList: MutableList<ItemModel> = ArrayList();
 
         for (i in 0..99) {
             itemModelList.add(ItemModel(String.format(Locale.US, "This is item, %d", i)))
--- app/src/main/java/com/example/jonathan/testrecyclerview/SimpleAdapter.kt
+++ app/src/main/java/com/example/jonathan/testrecyclerview/SimpleAdapter.kt
@@ -6,23 +6,28 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class SimpleAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private var models: List<ItemModel> = ArrayList<ItemModel>();
+    private var models: List<ItemModel> = ArrayList()
 
     constructor(itemModels: List<ItemModel>) : this() {
         models = itemModels
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        var view: View = LayoutInflater.from(parent.context).inflate(viewType, parent, false)
+        val view: View = LayoutInflater.from(parent.context).inflate(viewType, parent, false)
         return SimpleViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        var holder2 = holder as SimpleViewHolder
-        holder2.bindData(models.get(position))
+        val holder2 = holder as SimpleViewHolder
+        holder2.bindData(models[position])
     }
 
     override fun getItemCount(): Int {
         return models.size
+    }
+
+    /* ??? This fun has to be manually added: */
+    override fun getItemViewType(position: Int): Int {
+        return R.layout.item_in_recycler_view
     }
 }
